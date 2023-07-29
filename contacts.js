ad7addeeea79c9ee094e07a2f5ed77fa79cf41a7
DagmaraSobczak <daga.sobczak@gmail.com>
2023-07-29 08:15:07 +0200

    added contacts.js with functions

---
+++ contacts.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("error", error);
+    return [];
+  }
+}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.find((contact) => contact.id === contactId) || null;
+  } catch (error) {
+    console.error("error", error);
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    if (contacts.length === filteredContacts.length) return false;
+
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+    return true;
+  } catch (error) {
+    console.error("error", error);
+    return false;
+  }
+}
+async function addContact(name, email, phone) {
+  try {
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    const contacts = await listContacts();
+    contacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
+  } catch (error) {
+    console.error("error", error);
+    return null;
+  }
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
